9f31ff435a9a452e785f42187e19fc966d66cadd
Sven Riedel <sr@gimp.org>
2010-07-17 10:55:10 +0200

    Document public interface

--- lib/future_agent/future_agent.rb
+++ lib/future_agent/future_agent.rb
@@ -3,10 +3,26 @@ class FutureAgent
 
   @agents = {}
 
+=begin rdoc
+  Installs the SIGCHLD/SIGCLD signal handler for FutureAgent. Usually this
+  will not need to be invoked manually; it is invoked when the class is required.
+  If you overwrite the SIGCHLD handler somewhere after requiring this class
+  and you want to reset the default behaviour, you can call this method.
+
+  Caution: The signal handler will try to call the previously installed
+  handler for non-agent children that die. You can probably build a handler
+  invocation cycle if you aren't careful.
+=end
   def self.setup_signal_handler
     @old_handler = Signal.trap( "SIGCLD" ) { |signo| child_handler( signo ) }
   end
 
+=begin rdoc
+  Handles the death of a signel agent child identified by the PID.
+  Usually invoked automatically by the FutureAgent SIGCHLD/SIGCLD handler. 
+  If you overwrite this handler with your own however, you will need to 
+  call this method to clean up the agent state if an agent dies.
+=end
   def self.handle_pid( signal_number, pid )
     unless @agents.has_key?( pid )
       call_original_child_handler signal_number
@@ -20,7 +36,10 @@ class FutureAgent
 
   setup_signal_handler
 
-
+=begin rdoc
+  Returns a new agent instances that has been forked and is already running.
+  Retrieve the result from this agent by calling agent.result
+=end
   def self.fork( &block )
     agent = new( &block )
     child_pid = agent.send :fork!
@@ -28,11 +47,18 @@ class FutureAgent
     agent
   end
 
-  def initialize( &block )
+  def initialize( &block ) # :nodoc:
     @async_block = block
   end
 
-  # parent method
+=begin rdoc
+  Returns the result of the agent. If the result isn't available yet, calling
+  this method will block until the agent is done.
+
+  Calling this method will raise a FutureAgent::ChildDied exception if the
+  child process died with an exit status other than 0 (usually due to a raised
+  exception)
+=end
   def result
     return @result if defined?( @result )
 
